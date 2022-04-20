3d0fde62e6ba47f1268a94263e20837bdd917490
NawImNate <jnhatch3@gmail.com>
2022-04-20 14:36:50 -0400

    experimentation of compatibility, use of props

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  View,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Jared's React Native App!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Jared's React Native App! </Text>
+      <TouchableWithoutFeedback onPress={() => console.log("image tapped")}>
+        <Image
+          fadeDuration={3000}
+          blurRadius={1}
+          source={{
+            width: 300,
+            height: 400,
+            uri: "https://picsum.photos/200/300?grayscale",
+            // https://scontent-mia3-2.xx.fbcdn.net/v/t1.18169-9/530574_4379220372808_161015231_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=de6eea&_nc_ohc=a-93pWIc4rsAX-7TwGo&_nc_ht=scontent-mia3-2.xx&oh=00_AT-WC3nGDa_GaMFyPiufHRIHleu9SWtTbGkO3EhSmGWEkw&oe=62875FBB
+          }}
+        />
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 }
 
